174d46727e062ca9b0038da323547fa6ce96c4dd
CAT <jlwinterberg2001@gmail.com>
2025-05-19 15:26:27 +0200

    Added basic file structure

--- pages/index.php
+++ pages/index.php
@@ -6,7 +6,7 @@ $isLoggedIn = isset($_SESSION['user']);
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>Math Formula Hub</title>
+    <title>Reqoma</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <!-- Bootstrap 5 CDN -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
