0f68cd537047ab68d830bb381e86378b8d655c6d
CAT <jlwinterberg2001@gmail.com>
2025-05-20 15:10:55 +0200

    Changes to footer

--- includes/footer.php
+++ includes/footer.php
@@ -1,34 +1,37 @@
 </main>
 
-<footer>
-  <div class="footer-columns">
-    <div>
-      <h4>Company</h4>
-      <ul>
-        <li><a href="/about">About</a></li>
-        <li><a href="/blog">Blog</a></li>
-        <li><a href="/press">Press</a></li>
-      </ul>
+<footer class="bg-dark text-light py-4 mt-5">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-4">
+                <h5>Company</h5>
+                <ul class="list-unstyled">
+                    <li><a href="/about" class="text-light">About</a></li>
+                    <li><a href="/blog" class="text-light">Blog</a></li>
+                    <li><a href="/press" class="text-light">Press</a></li>
+                </ul>
+            </div>
+            <div class="col-md-4">
+                <h5>Support</h5>
+                <ul class="list-unstyled">
+                    <li><a href="/help" class="text-light">Help Center</a></li>
+                    <li><a href="/contact" class="text-light">Contact</a></li>
+                    <li><a href="/faq" class="text-light">FAQ</a></li>
+                </ul>
+            </div>
+            <div class="col-md-4">
+                <h5>Legal</h5>
+                <ul class="list-unstyled">
+                    <li><a href="/terms" class="text-light">Terms</a></li>
+                    <li><a href="/privacy" class="text-light">Privacy</a></li>
+                </ul>
+            </div>
+        </div>
+        <hr class="border-light">
+        <div class="text-center">
+            <small>&copy; 2025 Company Name. Built with ❤️</small>
+        </div>
     </div>
-    <div>
-      <h4>Support</h4>
-      <ul>
-        <li><a href="/help">Help Center</a></li>
-        <li><a href="/contact">Contact Support</a></li>
-        <li><a href="/faq">FAQ</a></li>
-      </ul>
-    </div>
-    <div>
-      <h4>Legal</h4>
-      <ul>
-        <li><a href="/terms">Terms of Service</a></li>
-        <li><a href="/privacy">Privacy Policy</a></li>
-      </ul>
-    </div>
-  </div>
-  <div class="footer-bottom">
-    <p>&copy; 2025 Company Name. Built with ❤️</p>
-  </div>
 </footer>
 
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
