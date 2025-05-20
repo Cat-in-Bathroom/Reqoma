6e8678523305ed39025ca9fc756a4b42c70b67d5
CAT <jlwinterberg2001@gmail.com>
2025-05-20 12:36:59 +0200

    saved changes

--- pages/index.php
+++ pages/index.php
@@ -2,17 +2,9 @@
 session_start();
 $isLoggedIn = isset($_SESSION['user']);
 ?>
+
 <?php include '../includes/header.php'; ?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <title>Reqoma</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <!-- Bootstrap 5 CDN -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
-</head>
-<body class="bg-light">
+
 
 <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
     <div class="container-fluid">
@@ -55,7 +47,5 @@ $isLoggedIn = isset($_SESSION['user']);
     <?php endif; ?>
 </div>
 
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
-</body>
-</html>
+
 <?php include '../includes/footer.php'; ?>
