65d012fd9e051a888a4687cb581cdf53260643aa
CAT <jlwinterberg2001@gmail.com>
2025-05-21 13:34:23 +0200

    Added Redirect to index.php

--- includes/header.php
+++ includes/header.php
@@ -27,7 +27,7 @@ if (session_status() === PHP_SESSION_NONE) {
 
 <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
     <div class="container-fluid">
-        <a class="navbar-brand" href="#">MathHub</a>
+        <a class="navbar-brand" href="index.php">Reqoma</a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
                 data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false"
                 aria-label="Toggle navigation">
