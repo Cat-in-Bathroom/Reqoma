885b0edf34902cc66285fd9fe9fd13d9fe6cc7af
CAT <jlwinterberg2001@gmail.com>
2025-06-11 14:23:21 +0200

    bug fix

--- pages/dashboard.php
+++ pages/dashboard.php
@@ -78,6 +78,7 @@ $formulas = $formulas_stmt->fetchAll(PDO::FETCH_ASSOC);
       <script>
         (function() {
   // Sidebar toggle logic
+  const dashboardFlex = document.getElementById('dashboard-flex');
   const sidebar = document.getElementById('sidebar');
   const sidebarHide = document.getElementById('sidebarHide');
   const sidebarShow = document.getElementById('sidebarShow');
@@ -86,6 +87,7 @@ $formulas = $formulas_stmt->fetchAll(PDO::FETCH_ASSOC);
     sidebar.classList.remove('sidebar-visible');
     sidebar.classList.add('sidebar-hidden');
     sidebarShow.style.display = 'block';
+    dashboardFlex.classList.add('hide-sidebar');
     sidebarHide.setAttribute('aria-expanded', 'false');
     sidebarShow.setAttribute('aria-expanded', 'true');
   });
@@ -94,6 +96,7 @@ $formulas = $formulas_stmt->fetchAll(PDO::FETCH_ASSOC);
     sidebar.classList.remove('sidebar-hidden');
     sidebar.classList.add('sidebar-visible');
     sidebarShow.style.display = 'none';
+    dashboardFlex.classList.remove('hide-sidebar');
     sidebarHide.setAttribute('aria-expanded', 'true');
     sidebarShow.setAttribute('aria-expanded', 'false');
   });
