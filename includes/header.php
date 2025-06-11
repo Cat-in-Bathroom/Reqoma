8d1ca9da4ae36354d78e4b270616367fe50da2bc
CAT <jlwinterberg2001@gmail.com>
2025-06-11 14:19:06 +0200

    shortend navbar

--- includes/header.php
+++ includes/header.php
@@ -109,6 +109,11 @@ if ($isLoggedIn) {
         margin-left: 0 !important;
       }
     }
+    .navbar {
+      padding-top: 0 !important;
+      padding-bottom: 0 !important;
+      min-height: 56px;
+    }
   </style>
     <!-- Select2 CSS -->
     <link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" rel="stylesheet" />
